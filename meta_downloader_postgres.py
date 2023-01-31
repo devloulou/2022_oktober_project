ccb9818006080fa48e4e7caf119121c40c69e9c1
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2023-01-31 20:35:28 +0100

    finalize postgres base solution

--- meta_downloader_postgres.py
+++ meta_downloader_postgres.py
@@ -1,1 +1,28 @@
+import tmdbsimple as tmdb
+
+from utils.file_handler import FileHandler
+from utils.search_module import SearchModule
 from utils.postgres_handler import PostgresHandler
+
+
+def meta_data_loader():
+    search_obj = tmdb.Search()
+    file_handler = FileHandler()
+    search = SearchModule(search_obj)
+    postgres = PostgresHandler()
+
+    movies = file_handler.get_data_from_folder()
+    # ebben a for ciklusban fog megtörténni minden lépés
+    # ami a metaadat letöltéssel / kiírással kapcsolatos
+    postgres.delete_data() #csak a testelés miatt
+    for movie in movies:
+        data = search.search_movie(movie)
+        # ide jönnek az adatbázis műveletek        
+        postgres.insert_movies(data)
+
+        file_handler.set_poster_path(movie)
+        image_binary = search.get_image_obj_in_binary()
+        file_handler.write_image(image_binary)
+
+if __name__ == '__main__':
+    meta_data_loader()
--- utils/sql_helper.py
+++ utils/sql_helper.py
@@ -16,7 +16,57 @@ vote_count integer
 )
 """
 
+create_tmdb_genre_ids = """
+create table tmdb_genre_ids (
+id integer,
+genre_id integer,
+CONSTRAINT fk_tmdb_movies
+      FOREIGN KEY(id) 
+	  REFERENCES tmdb_movies(id)
+)
+"""
 
 tmdb_tables = {
-    "tmdb_movies": create_tmdb_movies
+    "tmdb_movies": create_tmdb_movies,
+    "tmdb_genre_ids": create_tmdb_genre_ids
 }
+
+# sql template
+insert_tmdb_movies = """
+insert
+	into
+	public.tmdb_movies
+(adult,
+	backdrop_path,
+	id,
+	original_language,
+	original_title,
+	overview,
+	popularity,
+	poster_path,
+	release_date,
+	title,
+	video,
+	vote_average,
+	vote_count)
+values(:adult,
+:backdrop_path,
+:id,
+:original_language,
+:original_title,
+:overview,
+:popularity,
+:poster_path,
+:release_date,
+:title,
+:video,
+:vote_average,
+:vote_count);
+"""
+
+insert_tmdb_genre_ids = """
+insert into tmdb_genre_ids (
+    id,
+    genre_id
+) values (:id, :genre_id)
+"""
